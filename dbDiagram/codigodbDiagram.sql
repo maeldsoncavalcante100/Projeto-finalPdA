3e8801d8a54a6b6f14e8e46b1dd8e03c52cedc8c
maeldsoncavalcante100 <maeldson2015@gmail.com>
2024-06-27 08:17:42 -0300

    adicionando img do Diagrama conceitual e fazendo alterações no dbdiagram

--- dbDiagram/codigodbDiagram.sql
+++ dbDiagram/codigodbDiagram.sql
@@ -1,37 +1,35 @@
 Table Donor {
-  id int [pk, increment, not null]
-  name varchar(50)
-  cpf char(11) [not null]
-  email varchar(150)
-  cellphone char(11)
+  ID int [pk, increment] 
+  name varchar(255) [not null]
+  email varchar(255) [not null]
+  cpf varchar(14) [not null]
+  cellphone varchar(20) [not null]
 }
 
 Table Donation {
-  id int [pk, increment, not null]
-  item varchar(100) [not null]
-  category varchar(25) [not null]
+  ID int [pk, increment] 
+  item varchar(255) [not null]
+  category varchar(255) [not null]
   amount int [not null]
-  donor_id int [not null, ref: > Donor.id]
-  entry_date datetime
-}
-
-Table Donation_list {
-  id int [pk, increment, not null]
-  donation_id int [not null, ref: > Donation.id]
-  entry_date datetime [not null]
-}
-
-Table Stock {
-  id int [pk, increment, not null]
-  donation_id int [not null, ref: > Donation.id]
-  volunteer_id int [not null, ref: > Volunteer.id]
+  donor_id int [ref: > Donor.ID] 
+  entry_date date [not null]
 }
 
 Table Volunteer {
-  id int [pk, increment, not null]
-  name varchar(50)
-  cpf char(11)
-  role varchar(25)
+  ID int [pk, increment] 
+  name varchar(255) [not null]
+  cpf varchar(14) [not null]
+  role varchar(255) [not null]
+}
+
+Table Stock {
+  ID int [pk, increment] 
+  donation_id int [ref: > Donation.ID] 
+  volunteer_id int [ref: > Volunteer.ID] 
+  location varchar(255) [not null]
+  capacity int [not null]
+  entry_date date [not null]
+  operation varchar(255) [not null]
 }
 
 
